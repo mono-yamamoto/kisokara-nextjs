d284f2d0e5e57453b11c928b367db63548dd6b10
monosus_yamamoto <y.yamamoto@monosus.co.jp>
2023-08-26 19:59:01 +0900

    up: 吐き出しデータの整形

--- nextjs-sample/pages/index.tsx
+++ nextjs-sample/pages/index.tsx
@@ -5,38 +5,140 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-export const getStaticProps: GetStaticProps<{}> = async () => {
+export type Content = | {
+  type: 'paragraph' | 'quote' | 'heading_2' | 'heading_3';
+  text: string | null;
+}
+| {
+  type: 'code';
+  text: string | null;
+  language: string | null;
+};
+
+export type Post = {
+  id: string;
+  title: string | null;
+  slug: string | null;
+  createdTs: string | null;
+  lastEditedTs: string | null;
+  contents: Content[];
+}
+
+type StaticProps = {
+  post: Post | null;
+}
+
+export const getStaticProps: GetStaticProps<StaticProps> = async () => {
   const database = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID || "",
+    database_id: process.env.NOTION_DATABASE_ID || '',
     filter: {
       and: [
         {
-          property: "Published",
+          property: 'Published',
           checkbox: {
-            equals: true,
-          },
-        },
-      ],
+            equals: true
+          }
+        }
+      ]
     },
     sorts: [
       {
-        timestamp: "created_time",
-        direction: "descending",
-      },
-    ],
+        timestamp: 'created_time',
+        direction: 'descending'
+      }
+    ]
   });
-  // console.dir(database, { depth: null });
+
+  const page = database.results[1];
+  if(!page){
+    return{
+      props: {
+        post: null
+      }
+    };
+  }
+  if(!('properties' in page)){
+    return{
+      props: {
+        post: {
+          id: page.id,
+          title: null,
+          slug: null,
+          createdTs: null,
+          lastEditedTs: null,
+          contents: []
+        }
+      }
+    };
+  }
+
+  let title: string | null = null;
+  if(page.properties && page.properties['Name']?.type === 'title'){
+    title = page.properties['Name'].title[0]?.plain_text ?? null;
+  }
+
+  let slug: string | null = null;
+  if(page.properties && page.properties['Slug']?.type === 'rich_text'){
+    slug = page.properties['Slug'].rich_text[0]?.plain_text ?? null;
+  }
 
   const blocks = await notion.blocks.children.list({
-    block_id: "748a33ce-452b-4d4a-961e-36bf713921c7",
+    block_id: page.id
   });
 
-  console.dir(blocks, { depth: null });
+  const contents: Content[] = [];
+  blocks.results.forEach((block) => {
+    if(!('type' in block)){
+      return;
+    }
+    switch (block.type){
+      case 'paragraph':
+        contents.push({
+          type: 'paragraph',
+          text: block.paragraph.rich_text[0]?.plain_text ?? null
+        })
+        break;
+      case 'heading_2':
+        contents.push({
+          type: 'heading_2',
+          text: block.heading_2.rich_text[0]?.plain_text ?? null
+        })
+      break;
+      case 'heading_3':
+      contents.push({
+        type: 'heading_3',
+        text: block.heading_3.rich_text[0]?.plain_text ?? null
+      })
+      break;
+      case 'quote':
+        contents.push({
+          type: 'quote',
+          text: block.quote.rich_text[0]?.plain_text ?? null
+        })
+      break;
+      case 'code':
+        contents.push({
+          type: 'code',
+          text: block.code.rich_text[0]?.plain_text ?? null,
+          language: block.code.language
+        })
+    }
+  });
 
-  return {
-    props: {},
+  const post: Post = {
+    id: page.id,
+    title,
+    slug,
+    createdTs: page.created_time,
+    lastEditedTs: page.last_edited_time,
+    contents
   };
-};
+
+  console.dir(post, {depth: null});
+  return{
+    props: {post}
+  }
+}
 
 const Home = () => {
   return <div></div>;
