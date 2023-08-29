9a950a387a9c2d33dc7229bbb72accbd1c0c1ac0
monosus_yamamoto <y.yamamoto@monosus.co.jp>
2023-08-29 22:58:44 +0900

    up: notionのページ情報を取得してページに表示

--- nextjs-sample/pages/index.tsx
+++ nextjs-sample/pages/index.tsx
@@ -1,20 +1,21 @@
 import { Client } from "@notionhq/client";
 import { GetStaticProps } from "next";
-import {NextPage} from 'next'
+import { NextPage } from "next";
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-export type Content = | {
-  type: 'paragraph' | 'quote' | 'heading_2' | 'heading_3';
-  text: string | null;
-}
-| {
-  type: 'code';
-  text: string | null;
-  language: string | null;
-};
+export type Content =
+  | {
+      type: "paragraph" | "quote" | "heading_2" | "heading_3";
+      text: string | null;
+    }
+  | {
+      type: "code";
+      text: string | null;
+      language: string | null;
+    };
 
 export type Post = {
   id: string;
@@ -23,43 +24,43 @@ export type Post = {
   createdTs: string | null;
   lastEditedTs: string | null;
   contents: Content[];
-}
+};
 
 type StaticProps = {
   post: Post | null;
-}
+};
 
 export const getStaticProps: GetStaticProps<StaticProps> = async () => {
   const database = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID || '',
+    database_id: process.env.NOTION_DATABASE_ID || "",
     filter: {
       and: [
         {
-          property: 'Published',
+          property: "Published",
           checkbox: {
-            equals: true
-          }
-        }
-      ]
+            equals: true,
+          },
+        },
+      ],
     },
     sorts: [
       {
-        timestamp: 'created_time',
-        direction: 'descending'
-      }
-    ]
+        timestamp: "created_time",
+        direction: "descending",
+      },
+    ],
   });
 
   const page = database.results[1];
-  if(!page){
-    return{
+  if (!page) {
+    return {
       props: {
-        post: null
-      }
+        post: null,
+      },
     };
   }
-  if(!('properties' in page)){
-    return{
+  if (!("properties" in page)) {
+    return {
       props: {
         post: {
           id: page.id,
@@ -67,62 +68,62 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => {
           slug: null,
           createdTs: null,
           lastEditedTs: null,
-          contents: []
-        }
-      }
+          contents: [],
+        },
+      },
     };
   }
 
   let title: string | null = null;
-  if(page.properties && page.properties['Name']?.type === 'title'){
-    title = page.properties['Name'].title[0]?.plain_text ?? null;
+  if (page.properties && page.properties["Name"]?.type === "title") {
+    title = page.properties["Name"].title[0]?.plain_text ?? null;
   }
 
   let slug: string | null = null;
-  if(page.properties && page.properties['Slug']?.type === 'rich_text'){
-    slug = page.properties['Slug'].rich_text[0]?.plain_text ?? null;
+  if (page.properties && page.properties["Slug"]?.type === "rich_text") {
+    slug = page.properties["Slug"].rich_text[0]?.plain_text ?? null;
   }
 
   const blocks = await notion.blocks.children.list({
-    block_id: page.id
+    block_id: page.id,
   });
 
   const contents: Content[] = [];
   blocks.results.forEach((block) => {
-    if(!('type' in block)){
+    if (!("type" in block)) {
       return;
     }
-    switch (block.type){
-      case 'paragraph':
+    switch (block.type) {
+      case "paragraph":
         contents.push({
-          type: 'paragraph',
-          text: block.paragraph.rich_text[0]?.plain_text ?? null
-        })
+          type: "paragraph",
+          text: block.paragraph.rich_text[0]?.plain_text ?? null,
+        });
         break;
-      case 'heading_2':
+      case "heading_2":
         contents.push({
-          type: 'heading_2',
-          text: block.heading_2.rich_text[0]?.plain_text ?? null
-        })
-      break;
-      case 'heading_3':
-      contents.push({
-        type: 'heading_3',
-        text: block.heading_3.rich_text[0]?.plain_text ?? null
-      })
-      break;
-      case 'quote':
+          type: "heading_2",
+          text: block.heading_2.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case "heading_3":
         contents.push({
-          type: 'quote',
-          text: block.quote.rich_text[0]?.plain_text ?? null
-        })
-      break;
-      case 'code':
+          type: "heading_3",
+          text: block.heading_3.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case "quote":
         contents.push({
-          type: 'code',
+          type: "quote",
+          text: block.quote.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case "code":
+        contents.push({
+          type: "code",
           text: block.code.rich_text[0]?.plain_text ?? null,
-          language: block.code.language
-        })
+          language: block.code.language,
+        });
     }
   });
 
@@ -132,18 +133,70 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => {
     slug,
     createdTs: page.created_time,
     lastEditedTs: page.last_edited_time,
-    contents
+    contents,
   };
 
-  console.dir(post, {depth: null});
-  return{
-    props: {post}
-  }
-}
+  console.dir(post, { depth: null });
+  return {
+    props: { post },
+  };
+};
 
-const Home: NextPage<StaticProps> = ({post}) => {
-  console.log(post);
-  return <div></div>;
+import styles from "../styles/Home.module.css";
+import dayjs from "dayjs";
+
+const Home: NextPage<StaticProps> = ({ post }) => {
+  if (!post) return null;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.post1}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.timestampWrapper}>
+          <div>
+            <div className={styles.timestamp}>作成日時: {dayjs(post.createdTs).format("YYYY-MM-DD HH:mm:ss")}</div>
+            <div className={styles.timestamp}>更新日時: {dayjs(post.lastEditedTs).format("YYYY-MM-DD HH:mm:ss")}</div>
+          </div>
+        </div>
+        <div>
+          {post.contents.map((content, index) => {
+            const key = `${post.id}_${index}`;
+            switch (content.type) {
+              case "heading_2":
+                return (
+                  <h2 key={key} className={styles.heading2}>
+                    {content.text}
+                  </h2>
+                );
+              case "heading_3":
+                return (
+                  <h2 key={key} className={styles.heading3}>
+                    {content.text}
+                  </h2>
+                );
+              case "paragraph":
+                return (
+                  <h2 key={key} className={styles.paragraph}>
+                    {content.text}
+                  </h2>
+                );
+              case "code":
+                return (
+                  <pre className={`${styles.code} lang-${content.language}`}>
+                    <code>{content.text}</code>
+                  </pre>
+                );
+              case "quote":
+                return (
+                  <blockquote key={key} className={styles.quote}>
+                    {content.text}
+                  </blockquote>
+                );
+            }
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
