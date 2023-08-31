49feede636993e7e5ab88b090bc8588f760986f2
monosus_yamamoto <y.yamamoto@monosus.co.jp>
2023-08-31 21:43:06 +0900

    up: 複数ページ取得して表示

--- nextjs-sample/pages/index.tsx
+++ nextjs-sample/pages/index.tsx
@@ -27,7 +27,7 @@ export type Post = {
 };
 
 type StaticProps = {
-  post: Post | null;
+  posts: Post[];
 };
 
 export const getStaticProps: GetStaticProps<StaticProps> = async () => {
@@ -51,150 +51,152 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => {
     ],
   });
 
-  const page = database.results[1];
-  if (!page) {
-    return {
-      props: {
-        post: null,
-      },
-    };
-  }
-  if (!("properties" in page)) {
-    return {
-      props: {
-        post: {
-          id: page.id,
-          title: null,
-          slug: null,
-          createdTs: null,
-          lastEditedTs: null,
-          contents: [],
-        },
-      },
-    };
-  }
+  const posts: Post[] = [];
+  const blockResponses = await Promise.all(
+    database.results.map((page) => {
+      return notion.blocks.children.list({
+        block_id: page.id,
+      });
+    })
+  );
 
-  let title: string | null = null;
-  if (page.properties && page.properties["Name"]?.type === "title") {
-    title = page.properties["Name"].title[0]?.plain_text ?? null;
-  }
-
-  let slug: string | null = null;
-  if (page.properties && page.properties["Slug"]?.type === "rich_text") {
-    slug = page.properties["Slug"].rich_text[0]?.plain_text ?? null;
-  }
-
-  const blocks = await notion.blocks.children.list({
-    block_id: page.id,
-  });
-
-  const contents: Content[] = [];
-  blocks.results.forEach((block) => {
-    if (!("type" in block)) {
+  database.results.forEach((page, index) => {
+    if (!("properties" in page)) {
+      posts.push({
+        id: page.id,
+        title: null,
+        slug: null,
+        createdTs: null,
+        lastEditedTs: null,
+        contents: [],
+      });
       return;
     }
-    switch (block.type) {
-      case "paragraph":
-        contents.push({
-          type: "paragraph",
-          text: block.paragraph.rich_text[0]?.plain_text ?? null,
-        });
-        break;
-      case "heading_2":
-        contents.push({
-          type: "heading_2",
-          text: block.heading_2.rich_text[0]?.plain_text ?? null,
-        });
-        break;
-      case "heading_3":
-        contents.push({
-          type: "heading_3",
-          text: block.heading_3.rich_text[0]?.plain_text ?? null,
-        });
-        break;
-      case "quote":
-        contents.push({
-          type: "quote",
-          text: block.quote.rich_text[0]?.plain_text ?? null,
-        });
-        break;
-      case "code":
-        contents.push({
-          type: "code",
-          text: block.code.rich_text[0]?.plain_text ?? null,
-          language: block.code.language,
-        });
+    let title: string | null = null;
+    if (page.properties && page.properties["Name"]?.type === "title") {
+      title = page.properties["Name"].title[0]?.plain_text ?? null;
     }
+
+    let slug: string | null = null;
+    if (page.properties && page.properties["Slug"]?.type === "rich_text") {
+      slug = page.properties["Slug"].rich_text[0]?.plain_text ?? null;
+    }
+
+    const blocks = blockResponses[index];
+
+    const contents: Content[] = [];
+    blocks.results.forEach((block) => {
+      if (!("type" in block)) {
+        return;
+      }
+      switch (block.type) {
+        case "paragraph":
+          contents.push({
+            type: "paragraph",
+            text: block.paragraph.rich_text[0]?.plain_text ?? null,
+          });
+          break;
+        case "heading_2":
+          contents.push({
+            type: "heading_2",
+            text: block.heading_2.rich_text[0]?.plain_text ?? null,
+          });
+          break;
+        case "heading_3":
+          contents.push({
+            type: "heading_3",
+            text: block.heading_3.rich_text[0]?.plain_text ?? null,
+          });
+          break;
+        case "quote":
+          contents.push({
+            type: "quote",
+            text: block.quote.rich_text[0]?.plain_text ?? null,
+          });
+          break;
+        case "code":
+          contents.push({
+            type: "code",
+            text: block.code.rich_text[0]?.plain_text ?? null,
+            language: block.code.language,
+          });
+      }
+    });
+    posts.push({
+      id: page.id,
+      title,
+      slug,
+      createdTs: page.created_time,
+      lastEditedTs: page.last_edited_time,
+      contents,
+    });
   });
 
-  const post: Post = {
-    id: page.id,
-    title,
-    slug,
-    createdTs: page.created_time,
-    lastEditedTs: page.last_edited_time,
-    contents,
-  };
-
-  console.dir(post, { depth: null });
   return {
-    props: { post },
+    props: { posts },
   };
 };
 
 import styles from "../styles/Home.module.css";
 import dayjs from "dayjs";
+import prism from "prismjs";
+import { useEffect } from "react";
 
-const Home: NextPage<StaticProps> = ({ post }) => {
-  if (!post) return null;
+const Home: NextPage<StaticProps> = ({ posts }) => {
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.post1}>
-        <h1 className={styles.title}>{post.title}</h1>
-        <div className={styles.timestampWrapper}>
+      {posts.map((post) => (
+        <div className={styles.post1} key={post.id}>
+          <h1 className={styles.title}>{post.title}</h1>
+          <div className={styles.timestampWrapper}>
+            <div>
+              <div className={styles.timestamp}>作成日時: {dayjs(post.createdTs).format("YYYY-MM-DD HH:mm:ss")}</div>
+              <div className={styles.timestamp}>更新日時: {dayjs(post.lastEditedTs).format("YYYY-MM-DD HH:mm:ss")}</div>
+            </div>
+          </div>
           <div>
-            <div className={styles.timestamp}>作成日時: {dayjs(post.createdTs).format("YYYY-MM-DD HH:mm:ss")}</div>
-            <div className={styles.timestamp}>更新日時: {dayjs(post.lastEditedTs).format("YYYY-MM-DD HH:mm:ss")}</div>
+            {post.contents.map((content, index) => {
+              const key = `${post.id}_${index}`;
+              switch (content.type) {
+                case "heading_2":
+                  return (
+                    <h2 key={key} className={styles.heading2}>
+                      {content.text}
+                    </h2>
+                  );
+                case "heading_3":
+                  return (
+                    <h2 key={key} className={styles.heading3}>
+                      {content.text}
+                    </h2>
+                  );
+                case "paragraph":
+                  return (
+                    <h2 key={key} className={styles.paragraph}>
+                      {content.text}
+                    </h2>
+                  );
+                case "code":
+                  return (
+                    <pre className={`${styles.code} lang-${content.language}`}>
+                      <code>{content.text}</code>
+                    </pre>
+                  );
+                case "quote":
+                  return (
+                    <blockquote key={key} className={styles.quote}>
+                      {content.text}
+                    </blockquote>
+                  );
+              }
+            })}
           </div>
         </div>
-        <div>
-          {post.contents.map((content, index) => {
-            const key = `${post.id}_${index}`;
-            switch (content.type) {
-              case "heading_2":
-                return (
-                  <h2 key={key} className={styles.heading2}>
-                    {content.text}
-                  </h2>
-                );
-              case "heading_3":
-                return (
-                  <h2 key={key} className={styles.heading3}>
-                    {content.text}
-                  </h2>
-                );
-              case "paragraph":
-                return (
-                  <h2 key={key} className={styles.paragraph}>
-                    {content.text}
-                  </h2>
-                );
-              case "code":
-                return (
-                  <pre className={`${styles.code} lang-${content.language}`}>
-                    <code>{content.text}</code>
-                  </pre>
-                );
-              case "quote":
-                return (
-                  <blockquote key={key} className={styles.quote}>
-                    {content.text}
-                  </blockquote>
-                );
-            }
-          })}
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
